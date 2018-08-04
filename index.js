5a9578f05f20422cba091c07f8b9810030bfa086
jaredianmills <jaredianmills@gmail.com>
2018-08-04 10:56:26 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  let greeting;
+  if (distance <= 400) {
+    greeting = 'This one is on me!';
+  } else if (distance > 2000 && distance < 2500) {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    greeting = 'No can do.'
+  }
+  return greeting;
+}
+
+function ternaryCheckCity(city) {
+  let answer;
+  city === "NYC" ? answer = "Ok, sounds good." : answer = "No go.";
+  return answer;
+}
+
+function switchOnCharmFromTip(tip) {
+  let response;
+  switch(tip) {
+    case "generous":
+      response = "Thank you so much."
+      break;
+    case "not as generous":
+      response = "Thank you."
+      break;
+    default:
+      response = "Bye."
+  }
+  return response;
+}
